e780ecf1d1343f96fc2e8b66406d9c76eea68ebd
Anthony Vitale <vitale1313@gmail.com>
2013-07-15 21:56:35 -0400

    Initial commit

---
+++ src/net/evolutionarygames/BunchaStuff/BunchaStuff.java
@@ -0,0 +1,14 @@
+package net.evolutionarygames.BunchaStuff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BunchaStuff extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		
+	}
+	@Override
+	public void onDisable(){
+		
+	}
+}
